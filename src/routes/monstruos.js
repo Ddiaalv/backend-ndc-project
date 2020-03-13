fc34949044b932dc9ad762a8570b24084e4d59c1
David <ddiaalv@gmail.com>
2020-03-13 12:57:56 +0000

    Habilitar CORS

--- src/routes/monstruos.js
+++ src/routes/monstruos.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysqlConnection = require('../database');
 const router = express.Router();
 
-router.get('/listaMonstruos', (req, res) => {
+router.get('/listaMonstruos', (req, res, next) => {
   mysqlConnection.query(
     'SELECT id_monstruo, orden_menu, nombre, ruta, tipo, especie, vsfuego, vsagua, vsrayo, vshielo, vsdraco FROM monstruo ORDER BY orden_menu',
     (error, rows) => {
