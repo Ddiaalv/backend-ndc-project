23f417c66071b1d8811a3c897fd1f3d332b7b846
David <ddiaalv@gmail.com>
2020-03-13 13:01:31 +0000

    Activar CORS

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ const nodeca = express();
 // SETTINGS
 const PORT = 3010;
 //MIDDLEWARES - Se ejecuta antes de las rutas, por si queremos hacer alguna transformacion a la hora de obtener los datos.
-nodeca.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 nodeca.use(express.json());
 // ROUTES
 nodeca.use(require('./routes/monstruos'));
