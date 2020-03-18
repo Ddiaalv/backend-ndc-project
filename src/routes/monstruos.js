f2f80318cbc93f469aeac445566272eb2f289cdd
David <ddiaalv@gmail.com>
2020-03-18 18:37:08 +0000

    Nueva ruta monstruos, muestra todos los datos de un monstruo segun su ruta

--- src/routes/monstruos.js
+++ src/routes/monstruos.js
@@ -15,4 +15,17 @@ router.get('/listaMonstruos', (req, res, next) => {
   );
 });
 
+router.get('/monstruo/:id', (req, res, next) => {
+  mysqlConnection.query(
+    `SELECT id_monstruo, orden_menu, nombre, ruta, especie, tipo, rango, caracteristicas, notas, tamano_min, tamano_max, vsfuego, vsagua, vsrayo, vshielo, vsdraco, vsveneno, vssueno, vsparalisis, vsnitro, vsaturdimiento, elemento01, elemento01ruta, elemento02, resistencia01ruta, resistencia02, resistencia02ruta, resistencia03ruta, estado01ruta, estado02  FROM monstruo WHERE ruta = '${req.params.id}'`,
+    (error, rows) => {
+      if (!error) {
+        res.json(rows);
+      } else {
+        console.log(error);
+      }
+    }
+  );
+});
+
 module.exports = router;
