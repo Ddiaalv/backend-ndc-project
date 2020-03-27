1e565034b9418ab05da6a30849a356093f8061f2
David <ddiaalv@gmail.com>
2020-03-27 13:44:10 +0000

    Corregir error - añadir elementos, resistencias y estados

--- src/routes/monstruos.js
+++ src/routes/monstruos.js
@@ -17,7 +17,7 @@ router.get('/listaMonstruos', (req, res, next) => {
 
 router.get('/monstruo/:id', (req, res, next) => {
   mysqlConnection.query(
-    `SELECT id_monstruo, orden_menu, nombre, ruta, especie, tipo, rango, caracteristicas, notas, tamano_min, tamano_max, vsfuego, vsagua, vsrayo, vshielo, vsdraco, vsveneno, vssueno, vsparalisis, vsnitro, vsaturdimiento, elemento01, elemento01ruta, elemento02, resistencia01ruta, resistencia02, resistencia02ruta, resistencia03ruta, estado01ruta, estado02  FROM monstruo WHERE ruta = '${req.params.id}'`,
+    `SELECT id_monstruo, orden_menu, nombre, ruta, especie, tipo, rango, caracteristicas, notas, tamano_min, tamano_max, vsfuego, vsagua, vsrayo, vshielo, vsdraco, vsveneno, vssueno, vsparalisis, vsnitro, vsaturdimiento, elemento01, elemento02, resistencia01, resistencia02, resistencia03, estado01, estado02, estado03,  FROM monstruo WHERE ruta = '${req.params.id}'`,
     (error, rows) => {
       if (!error) {
         res.json(rows);
