d0ed4e76e1b0889cf7160379883e8d00e327e354
David <ddiaalv@gmail.com>
2020-03-31 05:04:02 +0100

    Cambio nombre columnas monstruos

--- src/routes/monstruos.js
+++ src/routes/monstruos.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/listaMonstruos', (req, res, next) => {
   mysqlConnection.query(
-    'SELECT id_monstruo, orden_menu, nombre, ruta, tipo, especie, vsfuego, vsagua, vsrayo, vshielo, vsdraco FROM monstruo ORDER BY orden_menu',
+    'SELECT id_monstruo, orden_menu, nombre, ruta, tipo, especie, fuego, agua, rayo, hielo, draco FROM monstruo ORDER BY orden_menu',
     (error, rows) => {
       if (!error) {
         res.json(rows);
@@ -17,7 +17,7 @@ router.get('/listaMonstruos', (req, res, next) => {
 
 router.get('/monstruo/:id', (req, res, next) => {
   mysqlConnection.query(
-    `SELECT id_monstruo, orden_menu, nombre, ruta, especie, tipo, rango, caracteristicas, notas, tamano_min, tamano_max, vsfuego, vsagua, vsrayo, vshielo, vsdraco, vsveneno, vssueno, vsparalisis, vsnitro, vsaturdimiento, elemento01, elemento02, resistencia01, resistencia02, resistencia03, estado01, estado02, estado03  FROM monstruo WHERE ruta = '${req.params.id}'`,
+    `SELECT id_monstruo, orden_menu, nombre, ruta, especie, tipo, rango, caracteristicas, notas, tamano_min, tamano_max, fuego, agua, rayo, hielo, draco, veneno, sueno, paralisis, nitro, aturdimiento, elemento01, elemento02, resistencia01, resistencia02, resistencia03, estado01, estado02, estado03  FROM monstruo WHERE ruta = '${req.params.id}'`,
     (error, rows) => {
       if (!error) {
         res.json(rows);
