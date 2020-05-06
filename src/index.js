4d4d54d5a0c1a8c13e74820f2b48565ddec633a0
David <ddiaalv@gmail.com>
2020-05-06 10:09:14 +0100

    Fixing CORS

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ const nodeca = express();
 // SETTINGS
 const PORT = process.env.PORT || 3010;
 //MIDDLEWARES - Se ejecuta antes de las rutas, por si queremos hacer alguna transformacion a la hora de obtener los datos.
-nodeca.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD'); // update to match the domain you will make the request from
+nodeca.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
 nodeca.use(express.json());
 // ROUTES
 nodeca.use(require('./routes/monstruos'));
