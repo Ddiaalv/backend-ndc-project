ffe0b82b386c9e668b52ee3dcd78b55c27ba91ba
David <ddiaalv@gmail.com>
2020-08-13 12:49:30 +0100

    :art: Improve swagger config and api routes

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,27 @@ import * as path from 'path';
 import monsterRoutes from './routes/monsters';
 import armorRoutes from './routes/armors';
 import weaponRoutes from './routes/weapons';
-import { options } from '../swagger-config';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
-// initialize swagger-jsdoc
+const swaggerDefinition = {
+  info: {
+    title: 'REST API for NODECA',
+    version: '1.0.0',
+    description: 'This is the REST API for NODECA',
+  },
+  host: 'localhost:3010',
+  basePath: '/',
+};
+export const options = {
+  swaggerDefinition,
+  apis: ['./docs/**/*.yaml'],
+};
+
 const swaggerSpec = swaggerJSDoc(options);
-
-// Create Express server
 const nodeca = express();
-
-// Settings
 const PORT = process.env.PORT || 3010;
 
-// Middlewares
 nodeca.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 nodeca.use(bodyParser.json());
 nodeca.use(bodyParser.urlencoded({ extended: true }));
@@ -31,12 +38,10 @@ nodeca.use((req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
-// Routes
 nodeca.use(monsterRoutes);
 nodeca.use(armorRoutes);
 nodeca.use(weaponRoutes);
 
-// Starting the server
 nodeca.listen(PORT, () => {
   console.log(`El server iniciado en el puerto: ${PORT}`);
   console.log('NODE_ENV: ', process.env.NODE_ENV);
